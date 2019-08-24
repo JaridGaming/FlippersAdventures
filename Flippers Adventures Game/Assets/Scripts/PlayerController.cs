c058f9e72cfb7ff36a75b54b618e97d28c2a871d
Dicky Chouryan <dicky_chow10@yahoo.com>
2019-08-24 17:35:37 -0400

    PlayerController. Make it switch faces

--- Flippers Adventures Game/Assets/Scripts/PlayerController.cs
+++ Flippers Adventures Game/Assets/Scripts/PlayerController.cs
@@ -2,13 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
     public float speed;
     private float moveInput;
 
     private Rigidbody rb;
 
+    private bool faceRight = true;
 
     void Start()
     {
@@ -20,5 +21,22 @@ public class NewBehaviourScript : MonoBehaviour
     {
         moveInput = Input.GetAxis("Horizontal");
         rb.velocity = new Vector3(moveInput * speed, rb.velocity.y);
+
+        if(faceRight == false && moveInput > 0)
+        {
+            SwitchFace();
+        }
+        else if(faceRight == true && moveInput < 0)
+        {
+            SwitchFace();
+        }
+    }
+
+    void SwitchFace()
+    {
+        faceRight = !faceRight;
+        Vector3 faces = transform.localEulerAngles;
+        faces.y *= -1;
+        transform.localEulerAngles = faces;
     }
 }

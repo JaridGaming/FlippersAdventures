02e9821865343e0f48a9d5a4731f853e4a82e181
JaridGaming <jaridkeen123@gmail.com>
2019-08-25 20:07:13 -0400

    Damage Volume and Health Script

---
+++ Flippers Adventures Game/Assets/Health.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public int maxHealth = 10;
+    public int currentHealth;
+    public Transform respawnMarker = null;
+    public bool triggerExplosion = false;
+    public int baseDamage = 10;
+
+    // Use this for initialization
+    void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public void ApplyDamage(int amount)
+    {
+        currentHealth -= amount;
+
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+
+            if (respawnMarker)
+            {
+                if (transform.GetComponent<Rigidbody>() != null)
+                    transform.GetComponent<Rigidbody>().velocity = Vector3.zero;
+
+                transform.position = respawnMarker.position;
+                currentHealth = maxHealth;
+            }
+            else
+            {
+                gameObject.SetActive(false);
+            }
+        }
+    }
+
+    internal void ApplyDamage(object baseDamage)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Flippers Adventures Game/Assets/Scripts/DamageVolume.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageVolume : MonoBehaviour
+{
+    public float damageInterval = 1;
+    private float currentInterval = 0;
+    public bool targetHere = false;
+
+    public GameObject PlayerSlot;
+
+    void Update()
+    {
+        if (targetHere == true)
+        {
+            if (PlayerSlot.GetComponent<Health>().currentHealth > 0)
+            {
+                currentInterval += Time.deltaTime;
+                if (currentInterval >= damageInterval)
+                {
+                    int baseDamage = 0;
+                    PlayerSlot.GetComponent<Health>().ApplyDamage(baseDamage);
+                    currentInterval = 0;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            if (other.GetComponent<Health>() != null)
+            {
+                PlayerSlot = other.gameObject;
+                targetHere = true;
+                //print(gameObject.name + " targeted " + myTarget.name + "!");
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == PlayerSlot)
+        {
+            targetHere = false;
+            PlayerSlot = null;
+            //print(gameObject.name + " lost its target.");
+        }
+    }
+}

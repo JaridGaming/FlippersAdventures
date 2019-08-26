51a6f14808b7711348f0d411a83d0b2290f9f4ca
JaridGaming <jaridkeen123@gmail.com>
2019-08-26 01:27:50 -0400

    Damage Volume (Why you hate me!!!)

---
+++ Flippers Adventures Game/Assets/Damage.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public bool Triggered = false;
+
+    public void OnTriggerEnter(Collider collider)
+    {
+        if (collider.tag == "Player" || collider.name == "Player")
+        {
+            HealthSystem healthSystem = new HealthSystem(100);
+
+            healthSystem.Damage(100);
+
+            Debug.Log("Health: " + healthSystem.GetHealth());
+
+            Triggered = true;
+        }
+    }
+}

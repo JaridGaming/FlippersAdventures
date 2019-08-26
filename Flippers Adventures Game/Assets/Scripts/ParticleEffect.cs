6166f29e4de29b2a6f13e19c821c569e32086805
Dicky Chouryan <dicky_chow10@yahoo.com>
2019-08-26 16:05:11 -0400

    create a statics class

---
+++ Flippers Adventures Game/Assets/Scripts/ParticleEffect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEffect : MonoBehaviour
+{
+    [SerializeField] float lifeTime = 1.0f;
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+    }
+}
---
+++ Flippers Adventures Game/Assets/Scripts/Static/GameplayStatics.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class GameplayStatics
+{
+    public static void DealDamage(GameObject DamagedObject, int Damage)
+    {
+        HealthComponent health = DamagedObject.GetComponent<HealthComponent>();
+        if (health)
+        {
+            health.ApplyDamage(Damage);
+        }
+    }
+}

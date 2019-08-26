908158b0f913b7997673f2b17ec21f813a20edc3
JaridGaming <jaridkeen123@gmail.com>
2019-08-26 01:16:06 -0400

    HealthSystem

--- Flippers Adventures Game/Assets/Scripts/Damage.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class Damage : MonoBehaviour
-{
-    public int baseDamage = 10;
-}
---
+++ Flippers Adventures Game/Assets/Scripts/GamerHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamerHandler : MonoBehaviour
+{
+    private void Start()
+    {
+        HealthSystem healthSystem = new HealthSystem(100);
+
+        Debug.Log("Health: "+healthSystem.GetHealth());
+    }
+}

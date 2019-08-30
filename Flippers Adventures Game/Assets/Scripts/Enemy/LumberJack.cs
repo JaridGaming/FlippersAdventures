2ffee3f1b22555b9c01fff0c7e88094ec2979419
Dicky Chouryan <dicky_chow10@yahoo.com>
2019-08-30 14:51:43 -0400

    lumberjack

---
+++ Flippers Adventures Game/Assets/Scripts/Enemy/LumberJack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LumberJack : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+    private Animator walkAnim;
+
+    public bool isWalled;
+    public Transform wallCheck;
+    public float checkRadius;
+    public LayerMask whatIsWall;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        walkAnim = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        isWalled = Physics.CheckSphere(wallCheck.position, checkRadius, whatIsWall);
+
+        rb.velocity = new Vector3(-speed, rb.velocity.y);
+
+        if(isWalled == true)
+        {
+            SwitchFace();
+        }
+    }
+
+    void Update()
+    {
+        walkAnim.SetBool("IsWalking", true);
+    }
+
+    void SwitchFace()
+    {
+        speed = -speed;
+        Vector3 faces = transform.localScale;
+        faces.x *= -1;
+        transform.localScale = faces;
+    }
+}
